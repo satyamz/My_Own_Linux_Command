1dfe26240f71cfdbcb178ed4ddc1ef7d8dc9bb66
Satyam Zode <satyamzode@gmail.com>
2015-02-21 00:23:51 +0530

    Added cation in script !

--- command_maker.sh
+++ command_maker.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-#This is main script . 
+# This is main script . 
+# Caution : Command duplication may occur . And you may loose original command in Linux system. 
+# Fixing this issue soon
 
 
-	if [ "$(id -u)" != "0" ]; then			#Checking whether root is running this script or not			
+	if [ "$(id -u)" != "0" ]; then					#Checking whether root is running this script or not			
 			echo "This script must be run as root" 1>&2
 			echo "Please try using root !"
 			exit 1
@@ -11,9 +13,9 @@
 	echo "Initializing script ..."
 
 	gcc -o $2 $1							#Compiling program 
-
+	
 	mv $2 /usr/bin							#Moving executable to bin 
 
-	echo "Done !"							
+	echo "Done !"												
 
 
